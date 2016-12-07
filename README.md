d59e95282c303f0e0be89a05adbb69d303f606e3
taicsuzu <taicsuzu@yahoo-corp.jp>
2016-12-07 22:51:12 +0900

    change sample json

--- README.md
+++ README.md
@@ -80,8 +80,8 @@ p.childs.size
 child1.parent.id
 # => 1
 
-p.childs.json
-# => [{"id": 1, "parent_id": 1}, {"id": 2, "parent_id": 1}, {"id": 3, "parent_id": 1}]
+p.json({include: :childs})
+# => {"id": 1, "childs": [{"id": 1, "parent_id": 1}, {"id": 2, "parent_id": 1}, {"id": 3, "parent_id": 1}]}
 ```
 See [sample code](https://github.com/tbrand/topaz/blob/master/samples/association.cr) for detail.  
 
---
+++ samples/readme.cr
@@ -0,0 +1,35 @@
+require "../src/topaz"
+
+Topaz::Db.setup("mysql://root@localhost/topaz") # For MySQL
+
+class SampleParent < Topaz::Model
+  columns # Empty columns
+  has_many( {model: SampleChild, as: childs, key: parent_id} )
+end
+
+class SampleChild < Topaz::Model
+  columns( # Define foreign key
+    {name: parent_id, type: Int32}
+  )
+  belongs_to( {model: SampleParent, as: parent, key: parent_id} )
+end
+
+SampleParent.drop_table
+SampleParent.create_table
+SampleChild.drop_table
+SampleChild.create_table
+
+p = SampleParent.create
+
+child1 = SampleChild.create(p.id)
+child2 = SampleChild.create(p.id)
+child3 = SampleChild.create(p.id)
+
+p.childs.size
+# => 3
+
+child1.parent.id
+# => 1
+
+p.json({include: :childs})
+# => {"id": 1, "childs": [{"id": 1, "parent_id": 1}, {"id": 2, "parent_id": 1}, {"id": 3, "parent_id": 1}]}
